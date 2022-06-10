01406c88a360dfdc0488d634d7ab99ae5faed495
gkpani97 <gkpani97@gmail.com>
2022-06-10 18:49:26 +0530

    Added Weather Report Fetching feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { WeatherFetchService } from './services/weather-fetch.service';
+import { WeatherReport } from './interfaces/weather-report';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-weather';
+  weatherLocations = [["751002", "in"], ["753002", "in"]];
+  weatherData:any = [];
+
+  constructor(private wthrFetch: WeatherFetchService) { }
+  
+  ngOnInit() {
+    for (let location of this.weatherLocations) {
+      this.wthrFetch.getWeatherReport(location[0], location[1]).subscribe((data) => {
+        this.weatherData.push(data);
+        console.log(data);
+      });
+    }
+  }
+
+  
+
+
 }
--- src/app/services/weather-fetch.service.ts
+++ src/app/services/weather-fetch.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { WeatherReport } from '../interfaces/weather-report';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WeatherFetchService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+    // owmUrl: string = `api.openweathermap.org/data/2.5/weather?zip=751002,in&appid=13fe5399b776d41191e4ef5ce0644a90`;
+
+  getWeatherReport(zip: string, countryCode: string) {
+    return this.http.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zip},${countryCode.toLowerCase()}&appid=13fe5399b776d41191e4ef5ce0644a90`); 
+  }
+
 }
