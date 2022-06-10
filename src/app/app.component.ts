9b5bbf6a9fe266634ce1416ab94cc6fe61b7299e
gkpani97 <gkpani97@gmail.com>
2022-06-10 21:10:06 +0530

    Added the location addition feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { WeatherFetchService } from './services/weather-fetch.service';
 import { WeatherReport } from './interfaces/weather-report';
+import {FormGroup,FormControl } from "@angular/forms";
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +12,19 @@ import { WeatherReport } from './interfaces/weather-report';
 })
 export class AppComponent {
   title = 'ng-weather';
+
+  inputObj = new FormGroup({
+    zip: new FormControl(''),
+    ccode: new FormControl('')
+  });
+
   weatherLocations = [["751002", "in"], ["753002", "in"]];
   weatherData:any = [];
 
   constructor(private wthrFetch: WeatherFetchService) { }
   
   ngOnInit() {
+    this.weatherData = [];
     for (let location of this.weatherLocations) {
       this.wthrFetch.getWeatherReport(location[0], location[1]).subscribe((data) => {
         this.weatherData.push(data);
@@ -24,7 +33,12 @@ export class AppComponent {
     }
   }
 
-  
+  AddZIPCC() {
+    var z:any = this.inputObj.controls.zip.value;
+    var cc:any = this.inputObj.controls.ccode.value
+    this.weatherLocations.push([z, cc]);
+    this.ngOnInit();
+  }
 
 
 }
